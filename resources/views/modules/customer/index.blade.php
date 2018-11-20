db1291fcc98a59930f3dde780f1f3138c0efff3b
Alexnguyen17 <Alexnguyen170395@gmail.com>
2018-11-20 08:54:26 +0700

    fff

---
+++ resources/views/modules/customer/index.blade.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+</head>
+<body>
+<h1 style="color: #ff5040">Danh sach khach hang</h1>
+<table border="1">
+    <thead>
+    <tr>
+        <th style="color: #2d995b">Id</th>
+        <th>UserName</th>
+        <th>Phone</th>
+        <th>Email</th>
+        <th>Action</th>
+    </tr>
+    </thead>
+    <tbody>
+    <tr>
+
+        <td style="color: #2d995b">1</td>
+        <td>Nguyen van A</td>
+        <td>0983692323</td>
+        <td>email.test@mail.com</td>
+        <td>
+            <a href="#">Show</a> | <a href="#">Edit</a> | <a href="#">Delete</a>
+        </td>
+    </tr>
+    <tr>
+        <td style="color: #2d995b">2</td>
+        <td>Nguyen van B</td>
+        <td>0983692323</td>
+        <td>email.test@mail.com</td>
+        <td>
+            <a href="#">Show</a> | <a href="#">Edit</a> | <a href="#">Delete</a>
+        </td>
+    </tr>
+    <tr>
+        <td style="color: #2d995b">3</td>
+        <td>Nguyen van C</td>
+        <td>0983692323</td>
+        <td>email.test@mail.com</td>
+        <td>
+            <a href="#">Show</a> | <a href="#">Edit</a> | <a href="#">Delete</a>
+        </td>
+    </tr>
+    <tr>
+        <td style="color: #2d995b">4</td>
+        <td>Nguyen van D</td>
+        <td>0983692323</td>
+        <td>email.test@mail.com</td>
+        <td>
+            <a href="#">Show</a> | <a href="#">Edit</a> | <a href="#">Delete</a>
+        </td>
+    </tr>
+    <tr>
+        <td style="color: #2d995b">5</td>
+        <td>Nguyen van E</td>
+        <td>0983692323</td>
+        <td>email.test@mail.com</td>
+        <td>
+            <a href="#">Show</a> | <a href="#">Edit</a> | <a href="#">Delete</a>
+        </td>
+    </tr>
+    </tbody>
+</table>
+</body>
+</html>
